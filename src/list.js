2a289eed9aae9db2d4d57bd3a82fd81b1c66f784
wqd182212 <10104911+wqd182212@user.noreply.gitee.com>
2022-05-25 17:59:26 +0800

    使用useSelector和useDispatch替换connect

--- src/list.js
+++ src/list.js
@@ -1,53 +1,57 @@
-import React from 'react';
-import './list.scss';
-import { Button, Input, List } from 'antd';
-import { connect } from 'react-redux';
-import { inputChange, clickButton, deleteItem } from './store/actionCreate';
+import React from "react";
+import "./list.scss";
+import { Button, Input, List } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+// import { inputChange, clickButton, deleteItem } from "./store/actionCreate";
+import { INPUT_CHANGE, CLICK_BUTTON, DELETE_ITEM } from "./store/action";
 
-const {Item} = List;
+const { Item } = List;
 
-const ListDemo = props => {
-    let {
-        inputValue,
-        listArr,
-        inputChange,
-        clickButton,
-        deleteItem,
-    } = props;
-    return (
-        <div className="list">
-            <Input
-                value={inputValue}
-                placeholder='请输入'
-                style={{ width: 160, margin: 10 }}
-                onChange={inputChange}
-            >
-            </Input>
-            <Button
-                type='primary'
-                onClick={clickButton}
-            >提交</Button>
-            <List
-                bordered
-                style={{width: 160, marginLeft: 10}}
-            >
-                {listArr.length ? listArr.map((item, index) => (
-                    <Item onClick={()=>deleteItem(index)} key={index}>{item}</Item>
-                )) : null}
-            </List>
-        </div>
-    );
-}
+const ListDemo = () => {
+  const dispatch = useDispatch();
+  const { input, arr } = useSelector((state) => state.listReducer);
+  return (
+    <div className="list">
+      <Input
+        value={input}
+        placeholder="请输入"
+        style={{ width: 160, margin: 10 }}
+        onChange={(e) =>
+          dispatch({
+            type: INPUT_CHANGE,
+            value: e.target.value,
+          })
+        }
+      ></Input>
+      <Button
+        type="primary"
+        onClick={() =>
+          dispatch({
+            type: CLICK_BUTTON,
+          })
+        }
+      >
+        提交
+      </Button>
+      <List bordered style={{ width: 160, marginLeft: 10 }}>
+        {arr.length
+          ? arr.map((item, index) => (
+              <Item
+                onClick={() =>
+                  dispatch({
+                    type: DELETE_ITEM,
+                    value: index,
+                  })
+                }
+                key={index}
+              >
+                {item}
+              </Item>
+            ))
+          : null}
+      </List>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({
-    inputValue: state.input,
-    listArr: state.arr,
-});
-
-const mapDispatchToProps = dispatch => ({
-    inputChange: (param) => dispatch(inputChange(param)),
-    clickButton: () => dispatch(clickButton()),
-    deleteItem: (param) => dispatch(deleteItem(param)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ListDemo);
+export default ListDemo;
